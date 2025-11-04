1d161b460e9c1f97d0bb65ed028f01d16f258891
nitazoso <nitalarasoo7@gmail.com>
2025-11-04 23:34:54 +0700

    untuk substr

--- 03_substr/main.cpp
+++ 03_substr/main.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+int main() {
+    string kalimat1("kucing hitam punya ekor ");
+    string kalimat2("pagi hari ayam berkokok");
+
+    cout << "substring\n";
+    cout << kalimat1 << endl;
+    cout << kalimat2 << endl;
+
+    //substring(index,panjang)
+    cout << kalimat1.substr(7,5) << endl;
+    cout << kalimat2.substr(10,4) << endl;
+
+    return 0;
+}
