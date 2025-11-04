35af2f4b54c6e28009563b74f0b75599e01b8ae6
nitazoso <nitalarasoo7@gmail.com>
2025-11-04 23:47:07 +0700

    erase
    Demonstrate string erase function in C++

--- 05_erase/main.cpp
+++ 05_erase/main.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+int main() {
+    string kalimat2("pagi hari ayam berkokok");
+
+    cout << "erase\n";
+    kalimat2.erase(5,4);
+    cout << kalimat2;
+
+    return 0;
+}
