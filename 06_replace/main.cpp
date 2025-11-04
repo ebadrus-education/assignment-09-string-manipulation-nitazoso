ee65c239df182380ff74ae219b4056a1f60fbd05
nitazoso <nitalarasoo7@gmail.com>
2025-11-04 23:49:16 +0700

    erase
    Replace 'ekor' with 'kalung' in the string.

--- 06_replace/main.cpp
+++ 06_replace/main.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+int main() {
+    string kalimat1("kucing hitam punya ekor ");
+
+    cout << "replace string\n";
+    kalimat1.replace(19,4,"kalung");
+    cout << kalimat1 << endl;
+
+    return 0;
+}
