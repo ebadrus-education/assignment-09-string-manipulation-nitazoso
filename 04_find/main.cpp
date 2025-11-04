432ceac903044b76663ee7d91f3794f9a8bc76c8
nitazoso <nitalarasoo7@gmail.com>
2025-11-04 23:43:22 +0700

    untuk find
    Add string search functionality to find positions of words and characters in sentences.

--- 04_find/main.cpp
+++ 04_find/main.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+int main() {
+    string kalimat1("kucing hitam punya ekor ");
+    string kalimat2("pagi hari ayam berkokok");
+
+    //mencari posisi
+    cout << "posisi hitam: ";
+    cout << kalimat1.find("hitam") << endl;
+    cout << "posisi hari";
+    cout << kalimat2.find("hari") << endl;
+
+    //mencari huruf i dalam variabel
+    cout << "i pada kucing";
+    int a = kalimat1.find("i"); 
+    cout << a << endl;
+    cout << "i pada hari";
+    cout << kalimat1.find("i", a+1) << endl;
+
+    return 0;
+}
